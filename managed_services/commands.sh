cadb610cbf8dd75fee332cc8148e00921c6bbfc3
kdnelson <kris.d.nelson@gmail.com>
2024-03-14 09:35:32 -0700

    Update commands.sh

--- managed_services/commands.sh
+++ managed_services/commands.sh
@@ -3,10 +3,23 @@
 
 terraform console
 
-# Test out the range function
-range(var.vpc_public_subnet_count)
+# Example functions and syntax
+# min(42,5,16)
+# lower("TACOCAT")
+# cidrsubnet(var.vpc_cidr_block, 8, 0)
+# cidrhost(cidrsubnet(var.vpc_cidr_block, 8, 0),5)
+
+# local.common_tags
+# local.common_tags["company"]
+# local.common_tags["missing"]
+# lookup(local.common_tags, "company", "Unknown")
+# lookup(local.common_tags, "missing", "Unknown")
+
+# Subnet range function
+# range(var.vpc_public_subnet_count)
+
 # Try it in a for expression
-[for subnet in range(var.vpc_public_subnet_count): cidrsubnet(var.vpc_cidr_block, 8, subnet)]
+# [for subnet in range(var.vpc_public_subnet_count): cidrsubnet(var.vpc_cidr_block, 8, subnet)]
 
 # Prepare config
 terraform init
@@ -14,13 +27,13 @@ terraform fmt -recursive
 terraform validate
 
 # For Linux and MacOS
-# Run command once and don't check in with access keys.
+# Run command once and don't check-in with access keys.
 # The AWS Provider will look and find these ENV variables.
 # export AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
 # export AWS_SECRET_ACCESS_KEY=YOUR_SECRET_ACCESS_KEY
 
 # For PowerShell
-# Run command once and don't check in with access keys.
+# Run command once and don't check-in with access keys.
 # The AWS Provider will look and find these ENV variables.
 # $env:AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY"
 # $env:AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
